14e88dd9d2f5ec266971bcaea3cd6e984b7e9762
FYGAR256 <fygar256@gmail.com>
2024-07-30 09:12:08 +0900

    Add files via upload

--- axx.py
+++ axx.py
@@ -1,11 +1,220 @@
 #!/usr/bin/python3
 import sys
-import expression
 pc=0
 capital="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower="abcdefghijklmnopqrstuvwxyz"
 alphabet=lower+capital
 symbols={}
+
+
+vars=[ 0 for i in range(26) ]
+
+def get_vars(s):
+    c=ord(s.upper())
+    return vars[c-ord('A')]
+
+def put_vars(s,v):
+    global vars
+    c=ord(s.upper())
+    vars[c-ord('A')]=v
+    return
+
+def err(m):
+    print(m)
+    return -1
+
+def factor(s,idx):
+    if s[idx]=='-':
+        (x,idx)=factor(s,idx+1)
+        x=-x
+    elif s[idx]=='~':
+        (x,idx)=factor(s,idx+1)
+        x=~x
+    else:
+        (x,idx)=factor1(s,idx)
+    return (x,idx)
+
+def factor1(s,idx):
+    x = 0
+
+    if s[idx]=='$':
+        idx+=1
+        x=pc
+    elif s[idx]=='0' and (s[idx+1]=='x' or s[idx+1]=='X'):
+        idx+=2
+        while(s[idx].upper() in "0123456789ABCDEF"):
+            x=16*x+(ord(s[idx])-0x30 if s[idx] in "0123456789" else ord(s[idx].upper())-0x41+10 )
+            idx+=1
+
+    elif s[idx] in "0123456789":
+        while(s[idx] in "0123456789"):
+            x=10*x+ord(s[idx])-0x30
+            idx+=1
+        a=1
+        if (s[idx]=='.'):
+            idx+=1
+            while(s[idx] in "0123456789"):
+                x+=(a/10)*(ord(s[idx])-0x30)
+                a/=10
+                idx+=1
+
+    elif s[idx] in "abcdefghijklmnopqrstuvwxyz":
+        x=vars[ord(s[idx].upper())-0x41]
+        idx+=1
+    elif s[idx]=='(':
+        (x,idx)=expression(s,idx+1)
+        if s[idx]!=')':
+            err("Missing ')'.")
+            idx=-1
+        else:
+            idx+=1
+    return (x,idx)
+
+def term0(s,idx):
+    (x,idx)=factor(s,idx)
+    while True:
+        if (s[idx]=='*'):
+            (t,idx)=factor(s,idx+1)
+            x*=t
+        elif (s[idx]=='/'):
+            (t,idx)=factor(s,idx+1)
+            if t==0:
+                err("Division by 0 error.")
+                idx=-1
+                x=-1
+            else:
+                x//=t
+        elif s[idx]=='%':
+            (t,idx)=factor(s,idx+1)
+            x=x%t
+        else:
+            break
+    return (x,idx)
+
+def term1(s,idx):
+    (x,idx)=term0(s,idx)
+    while True:
+        if (s[idx]=='+'):
+            (t,idx)=term0(s,idx+1)
+            x+=t
+        elif (s[idx]=='-'):
+            (t,idx)=term0(s,idx+1)
+            x-=t
+        else:
+            break
+    return (x,idx)
+
+def term2(s,idx):
+    (x,idx)=term1(s,idx)
+    while True:
+        if (s[idx]=='<' and s[idx+1]=='<'):
+            (t,idx)=term1(s,idx+2)
+            x<<=t
+        elif (s[idx]=='>' and s[idx+1]=='>'):
+            (t,idx)=term1(s,idx+2)
+            x>>=t
+        else:
+            break
+    return (x,idx)
+
+def term3(s,idx):
+    (x,idx)=term2(s,idx)
+    while True:
+        if (s[idx]=='&' and s[idx+1]!='&'):
+            (t,idx)=term2(s,idx+1)
+            x=int(x)&int(t)
+        else:
+            break
+    return (x,idx)
+
+
+def term4(s,idx):
+    (x,idx)=term3(s,idx)
+    while True:
+        if (s[idx]=='|' and s[idx+1]!='|'):
+            (t,idx)=term3(s,idx+1)
+            x=int(x)|int(t)
+        else:
+            break
+    return (x,idx)
+
+def term5(s,idx):
+    (x,idx)=term4(s,idx)
+    while True:
+        if (s[idx]=='^'):
+            (t,idx)=term4(s,idx+1)
+            x=int(x)^int(t)
+        else:
+            break
+    return (x,idx)
+
+def term6(s,idx):
+    (x,idx)=term5(s,idx)
+    while True:
+        if (s[idx]=='\''):
+            (t,idx)=term5(s,idx+1)
+            x=(x&~((~0)<<t))|((~0)<<t if (x>>(t-1)&1) else 0)
+        else:
+            break
+    return (x,idx)
+
+def term7(s,idx):
+    (x,idx)=term6(s,idx)
+    while True:
+        if (s[idx]=='<' and s[idx+1]=='='):
+            (t,idx)=term6(s,idx+2)
+            x=x<=t
+        elif (s[idx]=='<'):
+            (t,idx)=term6(s,idx+1)
+            x=x<t
+        elif (s[idx]=='>' and s[idx+1]=='='):
+            (t,idx)=term6(s,idx+2)
+            x=x>=t
+        elif (s[idx]=='>'):
+            (t,idx)=term6(s,idx+1)
+            x=x>t
+        elif (s[idx]=='=' and s[idx+1]=='='):
+            (t,idx)=term6(s,idx+2)
+            x=x==t
+        elif (s[idx]=='!' and s[idx+1]=='='):
+            (t,idx)=term6(s,idx+2)
+            x=x!=t
+        else:
+            break
+    return (x,idx)
+
+def term8(s,idx):
+    if s[idx]=='!':
+        (x,idx)=term8(s,idx+1)
+        x=not x
+    else:
+        (x,idx)=term7(s,idx)
+    return (x,idx)
+
+def term9(s,idx):
+    (x,idx)=term8(s,idx)
+    while True:
+        if (s[idx]=='&' and s[idx+1]=='&'):
+            (t,idx)=term8(s,idx+2)
+            x=x and t
+        else:
+            break
+    return (x,idx)
+
+def term10(s,idx):
+    (x,idx)=term9(s,idx)
+    while True:
+        if (s[idx]=='|' and s[idx+1]=='|'):
+            (t,idx)=term9(s,idx+2)
+            x=x or t
+        else:
+            break
+    return (x,idx)
+
+
+def expression(s,idx):
+    s+=chr(0)
+    return term10(s,idx)
 
 def issymbol(w):
     l=list(symbols.items())
@@ -28,7 +237,7 @@ def set_symbol(l):
     (w,idx)=getword(l,1)
     if l[idx]=='=':
         idx+=1
-        (v,idx)=expression.expression(l,idx)
+        (v,idx)=expression(l,idx)
         symbols[w.upper()]=v
 
 def remove_comment(l):
@@ -65,19 +274,15 @@ def readpat(fn):
     f.close()
     return [i for i in p if i]
 
-def hexp(n):
-    hexdigits="0123456789abcdef"
-    return "0x"+hexdigits[(n&0xf0)//16]+hexdigits[n%16]
-
 def makeobj(s):
-    global pc
     ch=','
     s+=chr(0)
     idx=0
     cnt=0
     while ch!=chr(0):
-        (x,idx)=expression.expression(s,idx)
-        print(f"{hexp(x)},",end='')
+        (x,idx)=expression(s,idx)
+        x=x&0xff
+        print("0x%02x," % x,end='')
         cnt+=1
         ch=s[idx]
         if ch==',':
@@ -107,16 +312,16 @@ def match(s,t):
         elif a==b:
             pass
         elif a in "abcdefghijklmn":
-            (v,idx_s)=expression.factor(s,idx_s)
+            (v,idx_s)=expression(s,idx_s)
             if idx_s==-1:
                 return False
-            expression.put_vars(t[idx_t],v)
+            put_vars(t[idx_t],v)
             idx_t+=1
             continue
         elif a in "opqrstuvwxyz":
             (w,idx_s)=getword(s,idx_s)
             if issymbol(w):
-                expression.put_vars(t[idx_t],symbols[w])
+                put_vars(t[idx_t],symbols[w])
             idx_t+=1
             continue
         elif a!=b:
@@ -129,10 +334,10 @@ def errorchk(s):
     s+=chr(0)
     ef=False
     while True:
-        (x,idx)=expression.expression(s,idx)
+        (x,idx)=expression(s,idx)
         if s[idx]==';':
             idx+=1
-            (e,idx)=expression.expression(s,idx)
+            (e,idx)=expression(s,idx)
         if x:
             ef=True
             print(f"error code:{e}")
@@ -146,7 +351,6 @@ def errorchk(s):
 def lineassemble(pat,line):
     if line=="" or line=="\n":
         return 0
-    expression.put_vars('z',pc)
     prev=''
     l=[i for i in line.replace('\n','').upper().split(' ') if i]
     idx=0
