08b67df1dfd4a1c6b47afc06c92d09d96ed48f98
Taisuke Maekawa <fygar256@outlook.jp>
2024-10-31 20:01:43 +0900

    Add files via upload

---
+++ hello.s
@@ -0,0 +1,16 @@
+section .text
+.export _start,len
+        .org 0x401000
+_start:
+        mov     edx, 0
+        mov     eax, 1      ; sys_write (01)
+        mov     edi, 1      ; stdout    (01)
+        mov     edx, len:    ; length    (13)
+        movq    esi, msg    ; address
+        syscall
+        mov     edi, 0      ; return 0
+        mov     eax, 60     ; sys_exit
+        syscall
+msg:     .ascii      "hello, world\n"
+len:     .equ     $$ - msg
+_end:
